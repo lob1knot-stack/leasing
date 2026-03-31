f6b20cf4d44cd9f5830a8e00cefda92865555934
Name <amirhamza.sayfiddinzoda@gmail.com>
2026-03-31 09:33:35 +0500

    fix router base for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import ScrollToTop from './components/ScrollToTop'; // We'll need this to scroll
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <div className="app">
         <Header />
